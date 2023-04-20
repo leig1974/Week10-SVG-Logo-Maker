7e307e1a687603721ec824019b92313961a2a408
Leyla Callahan <leyla.callahan@yahoo.com>
2023-04-19 22:41:14 -0400

    initial commit

---
+++ dist/readme.md
@@ -0,0 +1,51 @@
+# SVG Logo Maker
+
+## Description
+- questions regarding logo
+
+# Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Contribution](#contribution)
+4. [Test Instruction](#test_instruction)
+5. [License](#license)
+6. [Username](#username)
+7. [Github](#github)
+8. [Email](#email)
+
+
+## Installation
+- as per read me instruction file
+
+## Usage
+- as listed questions to be answered
+
+## Contribution
+- to record answers, to create video
+
+## Test Instruction
+- as per read me instructions file
+
+## License
+- Apache
+
+## Username
+- leig1974
+
+## Github
+- github.com/leig1974
+
+## Email
+- leyla.callahan@yahoo.cpom
+
+## Questions
+_If there are any questions_
+- yes
+---
+
+Please contact:
+
+Email: 
+[leyla.callahan@yahoo.cpom](mailto:leyla.callahan@yahoo.cpom);
+
+Website: [www.github.com](https://www.yahoo.com);
---
+++ index.js
@@ -0,0 +1,48 @@
+const inquirer = require("inquirer");
+const {writeFile} = require ("fs/promises");
+const {Triangle, Circle, Square, Svg} = require("./lib/shapes")
+const questions = [
+{
+    name:'text',
+    type: 'input',
+    message: 'text of Logo'
+},
+{
+    name:'textColor',
+    type: 'input',
+    message: 'textColor'
+},
+{
+    name:'shape',
+    type: 'list',
+    message: 'What is Logo shape?',
+    choices: ['triangle', 'circle', 'square']
+},
+{
+    name:'shapeColor',
+    type: 'input',
+    message: 'shapeColor'
+}
+]
+inquirer.prompt(questions)
+.then(answers => {
+    let shape = null
+    if (answers.shape === 'circle') {
+        shape = new Circle()
+        }
+    else if (answers.shape === 'square') {
+shape = new Square()
+    } else {
+        shape = new Triangle()
+    }
+    shape.setColor(answers.shapeColor) 
+
+    const svg = new Svg()
+    svg.setText(answers.color, answers.text)
+    svg.setShape(shape)
+    return writeFile("generatedLogo.svg", svg.render())
+}) .then(() => {
+    console.log("svg generated successfully")
+}) .catch((error) => {
+    console.log(error)
+})
